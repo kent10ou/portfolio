b7cc64a536ea789f21422e1e9827777cf7440c94
kent10ou <kent10ou@gmail.com>
2017-04-13 02:04:41 -0700

    send mail works!; still working on post req on frontend

--- server.js
+++ server.js
@@ -1,27 +1,15 @@
+'use strict';
+const config = require('./config');
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const app = express();
-
 const PORT = 8080;
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // Static Files
 app.use(express.static('public'));
-
-let transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'kent10ou@gmail.com',
-    pass: 'yourpass'
-  }
-});
-
-let mailOptions = {
-  from: '"Fred Foo" <foo@blurdybloop.com>',
-  to: 
-}
 
 // Routes
 app.get('/', (req, res) => {
@@ -29,7 +17,51 @@ app.get('/', (req, res) => {
 })
 
 app.post('/send_message', urlencodedParser, (req, res) => {
+  console.log('REQDATBODY: ', req.body);
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      service: 'Gmail',
+      auth: {
+          user: config.EMAIL,
+          pass: config.PASSWORD
+      }
+  });
 
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: req.body.name + ' <' + req.body.email + '>', // sender address
+      to: 'kent10ou@gmail.com', // list of receivers
+      subject: 'Portolio Page Contact Form', // Subject line
+      text: 'From: <' + req.body.email + '> Message: ' + req.body.message, // plain text body
+      // html: '<b>Hello world ?</b>' // html body
+  };
+
+  console.log('MAILOPTIONS: ', mailOptions);
+
+  transporter.verify(function(error, success) {
+      if (error) {
+          console.log(error);
+      } else {
+          console.log('Server is ready to take our messages');
+      }
+  });
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, response) => {
+      if (error) {
+          return console.log('ERROR IN TRANSPORTER: ', error);
+      } else {
+          console.log('RESPONSE: ', response);
+          console.log('Message %s sent: %s', response.messageId, response.response);
+          res.render('contact', { title: 'Kent Ou - Contact', msg: 'Message sent! Thank you.', err: false, page: 'contact' });
+      }
+  });
+
+  // res.send({ success: true });
+  res.end();
 })
 
 
