a5f081e29d46350bd5db325cd9ac4e31ecc8e387
kent10ou <kent10ou@gmail.com>
2017-06-08 02:12:23 -0700

    update index and server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 const fs = require('fs');
-//const config = require('./config');
+const config = require('./config');
 const http = require('http');
 const https = require('https');
 const express = require('express');
